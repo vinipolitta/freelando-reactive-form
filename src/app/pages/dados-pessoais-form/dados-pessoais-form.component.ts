381149b2afdfb48d01da22f4c423d02378f4ee15
vinipolitta <vinicius.politta1@gmail.com>
2025-07-25 03:03:19 -0300

    feat: validacoes e componentes reativos

--- src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
+++ src/app/pages/dados-pessoais-form/dados-pessoais-form.component.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { AbstractControl, AbstractControlOptions, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { Router } from '@angular/router';
+import { Cidade, Estado, IbgeService } from '../../shared/services/ibge.service';
+import { BehaviorSubject, Observable, of, startWith, switchMap, tap } from 'rxjs';
 
 export const senhasIguaisValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const senha = control.get('senha');
@@ -26,41 +28,17 @@ export const senhasIguaisValidator: ValidatorFn = (control: AbstractControl): Va
 export class DadosPessoaisFormComponent implements OnInit {
   dadosPessoaisForm!: FormGroup;
 
-  estados = [
-    { sigla: 'AC', nome: 'Acre' },
-    { sigla: 'AL', nome: 'Alagoas' },
-    { sigla: 'AP', nome: 'Amapá' },
-    { sigla: 'AM', nome: 'Amazonas' },
-    { sigla: 'BA', nome: 'Bahia' },
-    { sigla: 'CE', nome: 'Ceará' },
-    { sigla: 'DF', nome: 'Distrito Federal' },
-    { sigla: 'ES', nome: 'Espírito Santo' },
-    { sigla: 'GO', nome: 'Goiás' },
-    { sigla: 'MA', nome: 'Maranhão' },
-    { sigla: 'MT', nome: 'Mato Grosso' },
-    { sigla: 'MS', nome: 'Mato Grosso do Sul' },
-    { sigla: 'MG', nome: 'Minas Gerais' },
-    { sigla: 'PA', nome: 'Pará' },
-    { sigla: 'PB', nome: 'Paraíba' },
-    { sigla: 'PR', nome: 'Paraná' },
-    { sigla: 'PE', nome: 'Pernambuco' },
-    { sigla: 'PI', nome: 'Piauí' },
-    { sigla: 'RJ', nome: 'Rio de Janeiro' },
-    { sigla: 'RN', nome: 'Rio Grande do Norte' },
-    { sigla: 'RS', nome: 'Rio Grande do Sul' },
-    { sigla: 'RO', nome: 'Rondônia' },
-    { sigla: 'RR', nome: 'Roraima' },
-    { sigla: 'SC', nome: 'Santa Catarina' },
-    { sigla: 'SP', nome: 'São Paulo' },
-    { sigla: 'SE', nome: 'Sergipe' },
-    { sigla: 'TO', nome: 'Tocantins' }
-  ]
+  estado$!: Observable<Estado[]>;
+  cidade$!: Observable<Cidade[]>;
+
+  carregandoCidades$ = new BehaviorSubject<boolean>(false);
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
-    private cadastroService: CadastroService
-  ) {}
+    private cadastroService: CadastroService,
+    private ibgeService: IbgeService
+  ) { }
 
   ngOnInit(): void {
 
@@ -76,6 +54,42 @@ export class DadosPessoaisFormComponent implements OnInit {
       senha: ['', [Validators.required, Validators.minLength(6)]],
       confirmaSenha: ['', Validators.required]
     }, formOptions);
+
+    this.carregarEstados();
+    this.configurarListenerEstado();
+  }
+
+  private carregarEstados(): void {
+    this.estado$ = this.ibgeService.getEstados();
+  }
+
+  private configurarListenerEstado(): void {
+    const estadoControl = this.dadosPessoaisForm.get('estado');
+
+    if (estadoControl) {
+      this.cidade$ = estadoControl.valueChanges.pipe(
+        startWith(''),
+        tap(() => {
+          this.resetarCidades();
+          this.carregandoCidades$.next(true);
+        }),
+        switchMap((uf: string) => {
+          if (uf) {
+            return this.ibgeService.getCidadePorEstado(uf).pipe(
+              tap(() => this.carregandoCidades$.next(false))
+            );
+          }
+
+          this.carregandoCidades$.next(false);
+
+          return of([]);
+        }),
+      )
+    }
+  }
+
+  private resetarCidades(): void {
+    this.dadosPessoaisForm.get('cidade')?.setValue('');
   }
 
   onAnterior(): void {
